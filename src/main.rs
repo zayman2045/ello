8e379dda47aaa231660963669979a0b01ed6ccca
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-29 19:27:42 -0400

    Successfully read API key from local .env

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
 use std::env;
 
 fn main() {
-    match env::var("HOME") {
-        Ok(home) => {
-            println!("Your home directory is: {}", home);
-        },
+    // Load variables from .env file
+    dotenv().ok();
+
+    // Retrieve the API key from environment variables
+    match env::var("OPENAI_API_KEY") {
+        Ok(api_key) => {
+            println!("Successfully read API key: {}", api_key);
+            // Your logic here
+        }
         Err(_) => {
-            println!("Couldn't read HOME environment variable.");
+            println!("Couldn't read OPENAI_API_KEY from environment variables.");
         }
     }
 }
