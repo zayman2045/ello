b76c1418fdb948dc509956e94b6edcbc491e15db
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-29 22:28:32 -0400

    Make a request to the OpenAI API and print the response.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
-extern crate dotenv;
-
+use async_openai::{
+    config::OpenAIConfig,
+    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    Client,
+};
 use dotenv::dotenv;
 use std::env;
+use std::error::Error;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // Load variables from .env file
     dotenv().ok();
 
     // Retrieve the API key from environment variables
-    match env::var("OPENAI_API_KEY") {
-        Ok(api_key) => {
-            println!("Successfully read API key: {}", api_key);
-            // Your logic here
-        }
+    let api_key = match env::var("OPENAI_API_KEY") {
+        Ok(api_key) => api_key,
         Err(_) => {
             println!("Couldn't read OPENAI_API_KEY from environment variables.");
+            return Ok(());
+        }
+    };
+
+    let config = OpenAIConfig::new().with_api_key(api_key);
+    // .with_org_id("the-continental");
+
+    let client = Client::with_config(config);
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(512u16)
+        .model("gpt-3.5-turbo")
+        .messages([
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::System)
+                .content("You are a friend that holds conversation in Spanish, adjusting the difficulty level depending on the flow of the conversation.")
+                .build()?,
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content("¿Hola como estás?")
+                .build()?,
+        ])
+        .build()?;
+
+    let response = client.chat().create(request).await?;
+
+    for choice in response.choices {
+        match choice.message.content {
+            Some(content) => println!("{}", content),
+            None => println!("No content"),
         }
     }
+
+    Ok(())
 }
