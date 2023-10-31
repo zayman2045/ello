895318067610df3997f073745e05d1027a33b428
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-30 23:28:27 -0400

    Recive user input and send it as a request.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use async_openai::{
 use dotenv::dotenv;
 use std::env;
 use std::error::Error;
+use std::io;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -21,32 +22,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
+    // Create a client with the API key
     let config = OpenAIConfig::new().with_api_key(api_key);
-    // .with_org_id("the-continental");
-
     let client = Client::with_config(config);
 
-    let request = CreateChatCompletionRequestArgs::default()
+    let mut user_history = Vec::<String>::new();
+    let mut assistant_history = Vec::<String>::new();
+
+    println!("Welcome to the GPT-3 chatbot demo. Start a conversation. Type 'exit' to quit.");
+
+    loop {
+
+        let mut user_input = String::new();
+
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        user_input = user_input.trim().to_string();
+
+        if user_input == "exit" {
+            break;
+        }
+
+        // Make a request to the API
+        let request = CreateChatCompletionRequestArgs::default()
         .max_tokens(512u16)
         .model("gpt-3.5-turbo")
         .messages([
+            // Define the system personality
             ChatCompletionRequestMessageArgs::default()
                 .role(Role::System)
                 .content("You are a friend that holds conversation in Spanish, adjusting the difficulty level depending on the flow of the conversation.")
                 .build()?,
             ChatCompletionRequestMessageArgs::default()
                 .role(Role::User)
-                .content("¿Hola como estás?")
+                .content(user_input)
                 .build()?,
         ])
         .build()?;
 
-    let response = client.chat().create(request).await?;
+        let response = client.chat().create(request).await?;
 
-    for choice in response.choices {
-        match choice.message.content {
-            Some(content) => println!("{}", content),
-            None => println!("No content"),
+        for choice in response.choices {
+            match choice.message.content {
+                Some(content) => println!("{}", content),
+                None => println!("No content"),
+            }
         }
     }
 
