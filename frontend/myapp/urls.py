1095658334699ccf0ac4704426b765088309de55
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-08 16:18:48 -0500

    Define route and view for interacting with a specific assistant once selected.

--- frontend/myapp/urls.py
+++ frontend/myapp/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard', views.dashboard, name='dashboard'),
+    path('assistants/<str:assistant_id>', views.assistant_dashboard, name='assistant_dashboard'),
     path('build', views.build, name='build'),
 ]
--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,18 +1,47 @@
 import requests
 from django.shortcuts import render
 
+## VIEWS ##
+
+
 def home(request):
     return render(request, 'home.html')
 
 def dashboard(request):
+    assistant_list = get_assistant_list()
+
+    # Pass the data to the template
+    return render(request, 'dashboard.html', {'assistant_list': assistant_list})
+
+def build(request):
+    return render(request, 'build.html')
+
+def assistant_dashboard(request, assistant_id):
+    assistant_list = get_assistant_list()
+    assistant_info = get_assistant(assistant_id)
+
+    # Pass the data to the template
+    return render(request, 'dashboard.html', {'assistant_list': assistant_list, 'assistant_info': assistant_info})
+
+
+## HELPER FUNCTIONS ##
+
+# Requests the list of assistants from the backend
+def get_assistant_list():
     # Make a GET request to the specified URL
     response = requests.get('http://localhost:8080/assistants')
 
     # Parse the response as JSON
-    data = response.json()
+    assistant_list = response.json()
 
-    # Pass the data to the template
-    return render(request, 'dashboard.html', {'data': data})
+    return assistant_list
 
-def build(request):
-    return render(request, 'build.html')
+# Requests assistant info from the backend
+def get_assistant(assistant_id):
+    # Make a GET request to the specified URL
+    response = requests.get('http://localhost:8080/assistants/' + assistant_id)
+
+    # Parse the response as JSON
+    assistant_info = response.json()
+
+    return assistant_info
