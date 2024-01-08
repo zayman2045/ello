e8eca9e09ecadf0a4b171162ad63e5286c40fcf1
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-08 15:13:52 -0500

    Request list of assistants from the dashboard and display them within the nav bar.

--- frontend/myapp/views.py
+++ frontend/myapp/views.py
@@ -1,11 +1,18 @@
+import requests
 from django.shortcuts import render
 
-# Home page view
 def home(request):
     return render(request, 'home.html')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    # Make a GET request to the specified URL
+    response = requests.get('http://localhost:8080/assistants')
+
+    # Parse the response as JSON
+    data = response.json()
+
+    # Pass the data to the template
+    return render(request, 'dashboard.html', {'data': data})
 
 def build(request):
     return render(request, 'build.html')
