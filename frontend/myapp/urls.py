d6c43ebd22e470f9d684c4aaf1ffc6fa97a7358f
zayman2045 <xavierorlandogriffith@gmail.com>
2024-01-12 21:10:53 -0500

    Replace the term assistant with friend in all user-facing text.

--- frontend/myapp/urls.py
+++ frontend/myapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('assistants', views.dashboard, name='dashboard'),
-    path('assistants/create', views.assistant_create, name='assistant_create'),
-    path('assistants/<str:assistant_id>', views.assistant_dashboard, name='assistant_dashboard'),
-    path('assistants/<str:assistant_id>/delete', views.assistant_delete, name='assistant_delete'),
-    path('assistants/<str:assistant_id>/edit', views.assistant_edit, name='assistant_edit'),
+    path('friends', views.dashboard, name='dashboard'),
+    path('friends/create', views.assistant_create, name='assistant_create'),
+    path('friends/<str:assistant_id>', views.assistant_dashboard, name='assistant_dashboard'),
+    path('friends/<str:assistant_id>/delete', views.assistant_delete, name='assistant_delete'),
+    path('friends/<str:assistant_id>/edit', views.assistant_edit, name='assistant_edit'),
 ]
