ac8c198bb39bbecd3986fd895422e4ca56fcc4e8
zayman2045 <xavierorlandogriffith@gmail.com>
2024-02-22 17:17:44 -0500

    Handle boilerplate for error implementation using thiserror.

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -2,33 +2,27 @@
 
 use actix_web::{error::ResponseError, HttpResponse};
 use async_openai::error::OpenAIError;
-use std::fmt;
+use thiserror::Error;
 
-/// Custom error type for wrapping OpenAI errors.
-pub struct ElloError {
-    pub inner: OpenAIError,
-}
-
-impl fmt::Debug for ElloError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self.inner)
-    }
-}
-
-impl fmt::Display for ElloError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.inner)
-    }
-}
-
-impl From<OpenAIError> for ElloError {
-    fn from(error: OpenAIError) -> Self {
-        ElloError { inner: error }
+/// Custom error type for wrapping errors.
+#[derive(Error, Debug)]
+pub enum ElloError {
+    /// An error occurred with the OpenAI API.
+    #[error("An OpenAI error occurred")]
+    OpenAI {
+        #[from]
+        source: OpenAIError,
     }
 }
 
 impl ResponseError for ElloError {
     fn error_response(&self) -> HttpResponse {
-        HttpResponse::InternalServerError().json("An error occurred")
+        match self {
+            ElloError::OpenAI  { source } => {
+                match source {
+                    _ => HttpResponse::InternalServerError().json("An error occurred")
+                }
+            }
+        }
     }
 }
